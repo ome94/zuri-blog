21fdcfbb2b2f4162d1850b3c1e43b5802304245c
Mayowa Emmanuel Olabanji <mayowae@outlook.com>
2021-05-09 20:06:49 +0100

    Setup Heroku

--- task_crud/task_crud/settings.py
+++ task_crud/task_crud/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+# Configure Django App for Heroku.
+import os
+# import django_heroku
+# django_heroku.settings(locals())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -119,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
